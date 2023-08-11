b02c3ee693f72a5ea3717b8a0ffea2f105f3dc46
KAINAT MUSTAJAB AHMED <115812528+kmustajabjava@users.noreply.github.com>
2023-08-11 16:40:23 +0500

    Delete Frontend directory

--- Frontend/src/components/Filter.js
+++
@@ -1,7 +0,0 @@
-const Filter = ({filter, handleFilter}) => {
-  return <div>
-    filter shown with: &nbsp;  
-    <input value={filter} onChange={handleFilter}/>
-    </div>
-}
-export default Filter
